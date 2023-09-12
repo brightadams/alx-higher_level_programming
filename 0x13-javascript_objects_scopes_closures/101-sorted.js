11bb608f3123c3a53cf83e5fc068685153391a1f
Bright Adams <brightadams58@gmail.com>
2023-09-12 06:14:14 -0700

    3rd

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+// imports a dict and computes a dict by ocurrences
+const dict = require('./101-data').dict;
+const newDict = {};
+let lista = [];
+for (const [key, value] of Object.entries(dict)) {
+  if (!(value in newDict)) {
+    lista = [];
+  } else {
+    lista = newDict[value];
+  }
+  lista.push(key);
+  newDict[value] = lista;
+}
+console.log(newDict);
